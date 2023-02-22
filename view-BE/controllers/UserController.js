c0d6bd872a839e4b99062df0c139bf0c9c525ae6
tuananhnvl <tuananh.namvietluat@gmail.com>
2023-02-22 19:25:40 +0700

    sdf

--- view-BE/controllers/UserController.js
+++ view-BE/controllers/UserController.js
@@ -87,7 +87,7 @@ export const Login = async (req, res) => {
             }
         });
         res.cookie('refreshToken', refreshToken, {
-            httpOnly: true,
+            httpOnly: false,
             maxAge: 24 * 60 * 60 * 1000,
             sameSite: 'none'
         });
