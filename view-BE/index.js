1cd53fff7d0e28b2767f72eb5082055e8139e96c
tuananhnvl <tuananh.namvietluat@gmail.com>
2023-02-22 12:55:54 +0700

    d

--- view-BE/index.js
+++ view-BE/index.js
@@ -6,7 +6,19 @@ import UserRoute from "./routes/UserRoute.js";
 dotenv.config();
 const app = express();
 
-app.use(cors({ credentials:true, origin:true}));
+//app.use(cors({ credentials:true, origin:true}));
+app.use(cors({
+    origin: 'https://fe-htlaw.vercel.app/'
+}));
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'https://fe-htlaw.vercel.app');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+  });
+  
 app.use(cookieParser());
 app.use(express.json());
 app.use(UserRoute);
