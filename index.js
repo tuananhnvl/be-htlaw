726d0a2df244105ae3e984a202d20cc615b464c8
tuananhnvl <tuananh.namvietluat@gmail.com>
2023-02-22 12:50:39 +0700

    update tsting1

--- index.js
+++ index.js
@@ -10,6 +10,15 @@ const app = express();
 app.use(cors({
     origin: 'https://fe-htlaw.vercel.app/'
 }));
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'https://fe-htlaw.vercel.app');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+  });
+  
 app.use(cookieParser());
 app.use(express.json());
 app.use(UserRoute);
