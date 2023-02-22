c7277a868f110858175f2e949036be9c8227e968
tuananhnvl <tuananh.namvietluat@gmail.com>
2023-02-22 12:25:14 +0700

    update config db

--- view-BE/config/DBUser.js
+++ view-BE/config/DBUser.js
@@ -1,12 +1,22 @@
 import {Sequelize} from "sequelize";
 
-const dbhtlaw = new Sequelize('ht-law','root','',{
+/* const dbhtlaw = new Sequelize('ht-law','root','',{
     host: 'localhost',
     dialect: 'mysql',
     define: {
         timestamps: false // turn off timespamt
        
     }
+}); */
+
+const dbhtlaw = new Sequelize('sql12600163','sql12600163','wbRVF86vG8',{
+    host: 'sql12.freesqldatabase.com',
+    dialect: 'mysql',
+    define: {
+        timestamps: false // turn off timespamt
+       
+    }
 });
+
 
 export default dbhtlaw;
--- view-BE/config/Database.js
+++ view-BE/config/Database.js
@@ -1,10 +1,19 @@
 import {Sequelize} from "sequelize";
 
-const db = new Sequelize('datacustomer','root','',{
+/* const db = new Sequelize('datacustomer','root','',{
     host: 'localhost',
     dialect: 'mysql',
     define: {
         timestamps: false // turn off timespamt
+    }
+}); */
+
+const db = new Sequelize('sql12600163','sql12600163','wbRVF86vG8',{
+    host: 'sql12.freesqldatabase.com',
+    dialect: 'mysql',
+    define: {
+        timestamps: false // turn off timespamt
+       
     }
 });
 
